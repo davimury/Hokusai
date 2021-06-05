cde4aed906d66926d3a66f306081357beace38b8
Chris <mafradesouza.cs@gmail.com>
2021-06-05 17:10:23 -0300

    created trending tags

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
     <Header></Header>
-    <main class="flex justify-center mx-auto h-screen w-full md:max-w-4xl ">
+    <main class="flex justify-center mx-auto h-screen w-full md:max-w-4xl">
       <div class="w-full md:w-4/5 lg:w-3/5 h-screen p-3">
         <div class="w-full flex justify-center gap-5 bg-darkgray sticky top-0">
           <button
-            class="text-white border-b-2 hover:border-purple-600 focus:outline-none"
+            class="
+              text-white
+              border-b-2
+              hover:border-purple-600
+              focus:outline-none
+            "
             @click="changeFeedType(1)"
             :class="feedType == 1 ? 'border-purple-500' : 'border-lightergray'"
           >
             Seguindo
           </button>
           <button
-            class="text-white border-b-2 hover:border-purple-600 focus:outline-none"
+            class="
+              text-white
+              border-b-2
+              hover:border-purple-600
+              focus:outline-none
+            "
             @click="changeFeedType(2)"
             :class="feedType == 2 ? 'border-purple-500' : 'border-lightergray'"
           >
             Recomendados
           </button>
         </div>
-        <div
-          v-if="feedType == 1"
-          class=" w-full flex flex-col posts h-100"
-        >
+        <div v-if="feedType == 1" class="w-full flex flex-col posts h-100">
           <Post
             v-for="postData in postsData"
             :key="postData.post_id"
@@ -30,10 +37,7 @@
             class="my-4"
           ></Post>
         </div>
-        <div
-          v-if="feedType == 2"
-          class=" w-full flex flex-col posts h-100"
-        >
+        <div v-if="feedType == 2" class="w-full flex flex-col posts h-100">
           <Post
             v-for="postData in postsData"
             :key="postData.post_id"
@@ -44,8 +48,14 @@
       </div>
 
       <div id="right-bar" class="w-1/3 md:2/4 hidden lg:block h-screen p-3">
-        <SuggestedConection :suggestedConection="suggestedConection"></SuggestedConection>
+        <SuggestedConection
+          :suggestedConection="suggestedConection"
+          class="mb-4"
+        ></SuggestedConection>
+
+        <TrendingTags></TrendingTags>
       </div>
+
     </main>
     <img :src="imgDataUrl" />
     <div
@@ -56,7 +66,18 @@
       v-if="isFirstLogin"
     >
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        class="
+          flex
+          items-end
+          justify-center
+          min-h-screen
+          pt-4
+          px-4
+          pb-20
+          text-center
+          sm:block
+          sm:p-0
+        "
       >
         <div
           class="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"
@@ -68,11 +89,29 @@
           >&#8203;</span
         >
         <div
-          class="inline-block align-bottom overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          class="
+            inline-block
+            align-bottom
+            overflow-hidden
+            transform
+            transition-all
+            sm:my-8
+            sm:align-middle
+            sm:max-w-lg
+            sm:w-full
+          "
           v-on-clickaway="awayModalPost"
         >
           <div
-            class="bg-lightgray border border-lightgray rounded-lg block w-full mb-16 text-white"
+            class="
+              bg-lightgray
+              border border-lightgray
+              rounded-lg
+              block
+              w-full
+              mb-16
+              text-white
+            "
           >
             <form-wizard
               @on-complete="onComplete"
@@ -85,11 +124,10 @@
               :backButtonText="'Voltar'"
               :stepSize="'sm'"
             >
-              <tab-content
-                
-                :before-change="crop"
-              >
-              <h2 class="text-center mb-4 font-semibold">Escolha sua foto de perfil</h2>
+              <tab-content :before-change="crop">
+                <h2 class="text-center mb-4 font-semibold">
+                  Escolha sua foto de perfil
+                </h2>
                 <vue-croppie
                   ref="croppieRef"
                   :enableExif="true"
@@ -100,10 +138,25 @@
                 <input type="file" @change="croppie" />
               </tab-content>
               <tab-content>
-                <div class="flex flex-wrap content-center justify-center ">
-                  <h2 class="text-center mb-4 font-semibold">Escolha categorias que você tem interesse</h2>
+                <div class="flex flex-wrap content-center justify-center">
+                  <h2 class="text-center mb-4 font-semibold">
+                    Escolha categorias que você tem interesse
+                  </h2>
                   <div
-                    class="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-purple-500 border-purple-500 hover:bg-purple-500 hover:text-purple-600 cursor-default"
+                    class="
+                      text-xs
+                      mr-2
+                      my-1
+                      uppercase
+                      tracking-wider
+                      border
+                      px-2
+                      text-purple-500
+                      border-purple-500
+                      hover:bg-purple-500
+                      hover:text-purple-600
+                      cursor-default
+                    "
                     v-for="tag in recomendedTags"
                     :key="tag.id"
                     :id="tag.id"
@@ -128,6 +181,7 @@ import Post from "./Post.vue";
 import { FormWizard, TabContent } from "vue-form-wizard";
 import "vue-form-wizard/dist/vue-form-wizard.min.css";
 import SuggestedConection from "./SuggestedConection.vue";
+import TrendingTags from "./TrendingTags.vue";
 import axios from "axios";
 
 import Header from "./Header.vue";
@@ -143,6 +197,7 @@ export default {
     Footer,
     FormWizard,
     TabContent,
+    TrendingTags,
   },
   directives: {
     onClickaway: onClickaway,
@@ -164,7 +219,7 @@ export default {
       postsData: [],
       recomendedTags: [],
       selectedTags: [],
-      suggestedConection: []
+      suggestedConection: [],
     };
   },
   computed: {
@@ -235,7 +290,7 @@ export default {
       });
     },
     onComplete: async function () {
-      console.log(this.selectedTags)
+      console.log(this.selectedTags);
       axios({
         method: "post",
         url: "/profile/image",
@@ -243,8 +298,8 @@ export default {
           base: this.croppieImage,
         },
       });
-      this.selectedTags.forEach(element => {
-        console.log(element)
+      this.selectedTags.forEach((element) => {
+        console.log(element);
         axios({
           method: "post",
           url: "/profile/add/tag",
@@ -253,7 +308,7 @@ export default {
           },
         });
       });
-      
+
       await this.setFirstLogin(false);
     },
   },
@@ -296,12 +351,12 @@ export default {
   width: 0px;
   background: transparent; /* make scrollbar transparent */
 }
-.stepTitle{
+.stepTitle {
   color: #3b3b3b;
   line-height: 1.2rem;
-  margin-top: .7rem;
+  margin-top: 0.7rem;
 }
-.vue-form-wizard .wizard-icon-circle{
+.vue-form-wizard .wizard-icon-circle {
   background-color: #1e1e1e;
   color: white;
 }
---
+++ frontend/src/components/TrendingTags.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="
+      bg-lightgray
+      border border-lightgray
+      rounded-lg
+      block
+      w-full
+      text-white
+      lg:float-left
+      max-w-xs
+    "
+  >
+    <div class="flex flex-col px-4 py-3">
+      <h1 class="mb-5 text-lg font-semibold">
+        <span class="text-purple-500 frontend">#</span> Tags em alta
+      </h1>
+      <div class="mt-1 flex flex-col w-100">
+        <div class="inline-block" v-for="(tag, index) in tags" :key="tag">
+          <div class="mb-2">
+            <span class="mr-4">{{ index + 1 }}</span>
+            <span
+              class="
+                text-xs
+                mr-2
+                my-1
+                uppercase
+                tracking-wider
+                border
+                px-2
+                text-purple-500
+                border-purple-500
+                hover:text-purple-600
+                hover:border-purple-600
+                cursor-pointer
+              "
+              >{{ tag }}</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrendingTags",
+  data() {
+    return {
+      tags: ["boku", "teste"],
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>
