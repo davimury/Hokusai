de2ccef3012fc4add0a8b7b0a72deaafaeb1670a
Chris <mafradesouza.cs@gmail.com>
2021-06-11 16:48:56 -0300

    update chat

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="h-screen ">
+  <div class="h-screen">
     <Header></Header>
-    <main class="flex h-content">
-      <div id="contactsSidebar"></div>
-      
-      <div class="h-full flex flex-col md:max-w-4xl bg-lightgray text-white">
-        <div class="flex items-center px-4 py-3">
-          <img
-            class="h-12 w-12 rounded-full"
-            src="https://picsum.photos/id/1027/150/150"
-          />
-          <div class="ml-3">
-            <span class="text-lg font-semibold">Chris</span>
-          </div>
-        </div>
-        
-        
-      </div>
+    <main class="">
+      <chat-window
+        :current-user-id="currentUserId"
+        :rooms="rooms"
+        :messages="messages"
+        :height="height"
+        show-files="false"
+        show-audio="false"
+        text-formatting="false"
+        theme="dark"
+      />
     </main>
     <Footer></Footer>
   </div>
@@ -25,11 +20,93 @@
 <script>
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
+import ChatWindow from "vue-advanced-chat";
+import "vue-advanced-chat/dist/vue-advanced-chat.css";
 export default {
   name: "Chat",
   components: {
     Header,
     Footer,
+    ChatWindow,
+  },
+  data() {
+    return {
+      rooms: [
+        {
+          roomId: 1,
+          roomName: "Room 1",
+          avatar: "assets/imgs/people.png",
+          unreadCount: 4,
+          index: 3,
+          lastMessage: {
+            content: "Last message received",
+            senderId: 1234,
+            username: "John Doe",
+            timestamp: "10:20",
+            saved: true,
+            distributed: false,
+            seen: false,
+            new: true,
+          },
+          users: [
+            {
+              _id: 1234,
+              username: "John Doe",
+              avatar: "assets/imgs/doe.png",
+              status: {
+                state: "online",
+                lastChanged: "today, 14:30",
+              },
+            },
+            {
+              _id: 4321,
+              username: "John Snow",
+              avatar: "assets/imgs/snow.png",
+              status: {
+                state: "offline",
+                lastChanged: "14 July, 20:00",
+              },
+            },
+          ],
+          typingUsers: [4321],
+        },
+      ],
+      messages: [
+        {
+          _id: 7890,
+          content: "message 1",
+          senderId: 1234,
+          username: "John Doe",
+          avatar: "assets/imgs/doe.png",
+          date: "13 November",
+          timestamp: "10:20",
+          system: false,
+          saved: true,
+          distributed: true,
+          seen: true,
+          disableActions: false,
+          disableReactions: false,
+          file: {
+            name: "My File",
+            size: 67351,
+            type: "png",
+            audio: true,
+            duration: 14.4,
+            url: "https://firebasestorage.googleapis.com/...",
+            preview: "data:image/png;base64,iVBORw0KGgoAA...",
+          },
+          reactions: {
+            wink: [
+              1234, // USER_ID
+              4321,
+            ],
+            laughing: [1234],
+          },
+        },
+      ],
+      currentUserId: 1234,
+      height: "calc(100vh - 60.8px)",
+    };
   },
 };
 </script>
